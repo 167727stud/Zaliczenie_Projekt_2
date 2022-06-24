4c18938ae7c3a19c70c794cfb5b472270dba6ca5
167727 <100698238+167727stud@users.noreply.github.com>
2022-06-24 16:54:33 +0000

    Dobra wszystko działa

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main implements Runnable{
 JFrame f;  
 Thread t=null;  
 int godzina=0, minuta=0, sekunda=0;  
-String Czas = ""; // string wyświetlający czas 
+String Czas = ""; // string wyświetlający czas zapełniony spacją bo replit się burzy i nie lubie podkreślonych rzeczy
 JButton b;  
   
 Main(){  
@@ -19,7 +19,7 @@ Main(){
   // b.setBounds(odl. od lewej,odl. od góry,wymiar X,Wymiar Y);
 
 
-  // Wymiary okna
+  // Wymiary okna i czy je widać bo domyślnym stanem jest niewidoczny -_-
     f.add(b);  
     f.setSize(150,120);  
     f.setLayout(null);  
@@ -30,14 +30,14 @@ Main(){
       try {  
          while (true) {  
   
-            Calendar cal = Calendar.getInstance();  
+            Calendar cal = Calendar.getInstance(); //Zapętlenie zegara w przypadku dotarcia do do maksymalnej wartości 
             godzina = cal.get( Calendar.HOUR_OF_DAY );  
             if ( godzina > 12 ) godzina -= 12;  
             minuta = cal.get( Calendar.MINUTE );  
             sekunda = cal.get( Calendar.SECOND );  
   
             SimpleDateFormat formatter = new 
-            SimpleDateFormat("hh:mm:ss");  //import java.text.*; //Zwraca czas w formacie 12 godzinnym :(
+            SimpleDateFormat("hh:mm:ss");  //import java.text.*; //Zwraca czas w formacie 12 godzinnym :( i to zawsze dwie godziny do tyłu , podejrzewam że serwery mają gdzieś w angli lub po prostu używają greenwich meantime który w w porównianiu do CEST właśnie jest do tyłu o tyle godzin.
             Date date = cal.getTime();  
             Czas = formatter.format( date );  
   
